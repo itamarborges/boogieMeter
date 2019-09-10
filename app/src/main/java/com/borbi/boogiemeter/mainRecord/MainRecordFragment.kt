158329456110581376ab65108cff58999ee59bbb
Itamar Borges da Silva Filho <itamarborges@gmail.com>
2019-09-10 11:55:40 -0300

    Removing action bar Navigating from one fragment to another one

--- app/src/main/java/com/borbi/boogiemeter/mainRecord/MainRecordFragment.kt
+++ app/src/main/java/com/borbi/boogiemeter/mainRecord/MainRecordFragment.kt
@@ -6,10 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.borbi.boogiemeter.R
 import com.borbi.boogiemeter.databinding.FragmentMainRecordBinding
 
 class MainRecordFragment : Fragment() {
+
+    private lateinit var viewModel: MainRecordViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,15 +23,21 @@ class MainRecordFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-//        val view = DataBindingUtil.inflate(R.layout.fragment_main_record, container, false)
-
-//        viewTransactionsButton.setOnClickListener { view ->
-//            view.findNavController().navigate(R.id.viewTransactionsAction)
-//        }
 
         val binding: FragmentMainRecordBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_main_record, container, false)
+
+        viewModel = ViewModelProviders.of(this).get(MainRecordViewModel::class.java)
+
+        binding.mainRecordViewModel = viewModel
+
+        viewModel.navigateToRecording.observe(this, Observer {
+            if (it == true) { // Observed state is true.
+                this.findNavController().navigate(MainRecordFragmentDirections.actionMainRecordToRecording())
+                viewModel.doneMoveToRecording()
+            }
+        })
+
         return binding.root
 
     }
---
+++ app/src/main/java/com/borbi/boogiemeter/mainRecord/MainRecordViewModel.kt
@@ -0,0 +1,24 @@
+package com.borbi.boogiemeter.mainRecord
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainRecordViewModel : ViewModel() {
+
+    private val _navigateToRecording = MutableLiveData<Boolean?>()
+
+    val navigateToRecording: LiveData<Boolean?>
+        get() = _navigateToRecording
+
+
+    fun moveToRecording() {
+        _navigateToRecording.value = true
+    }
+
+    fun doneMoveToRecording() {
+        _navigateToRecording.value = false
+    }
+
+
+}
